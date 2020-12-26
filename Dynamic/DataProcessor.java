9365b50905f337cd975763e06f7387bc1994731d
V1tali1ty <vitalikkryvonosiuk@gmail.com>
2020-12-26 23:18:50 +0200

    Dynamic massively improved (not really)

--- Dynamic/DataProcessor.java
+++ Dynamic/DataProcessor.java
@@ -1,57 +1,171 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.ads7.Dynamic;
 
-/**
- *
- * @author START
- */
+import java.util.ArrayList;
+
 public class DataProcessor {
 
-    private int isBigger(int[][] array, int i, int b) {
-        if (array[i][b] >= array[i][b + 1]) {
-            return array[i][b];
+    private int isBigger (ArrayList array, int b) {
+        if ((int) array.get(b) <= (int)array.get(b+1)) {
+            return (int) array.get(b+1);
         } else {
-            return array[i][b + 1];
+            return (int) array.get(b);
         }
     }
 
-    private int isSmaller(int[][] array, int i, int b) {
-        if (array[i][b] <= array[i][b + 1]) {
-            return array[i][b];
+    private int isSmaller(ArrayList array, int b) {
+        if ((int) array.get(b) <= (int)array.get(b+1)) {
+            return (int) array.get(b);
         } else {
-            return array[i][b + 1];
+            return (int) array.get(b+1);
         }
     }
 
     
+    // Big for mid
+    int calculatorBigMid(int[][] array) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(array.length);
+        for(int i =0; i<array.length; i++) {
+            sums.add(new ArrayList());
+            for(int b =0; b<array[i].length; b++) {
+                sums.get(i).add(array[i][b]);
+            }
+        }
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
+
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isBigger(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
+            }
+        }
+        
+        return sums.get(0).get(0);
+
+    }
+    // Small for mid
+    int calculatorSmallMid(int[][] array) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(array.length);
+        for(int i =0; i<array.length; i++) {
+            sums.add(new ArrayList());
+            for(int b =0; b<array[i].length; b++) {
+                sums.get(i).add(array[i][b]);
+            }
+        }
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
+
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isSmaller(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
+            }
+        }
+        
+        return sums.get(0).get(0);
+    }
     
-    int calculatorBig(int[][] array) {
-
-        for (var i = (array.length - 2); i >= 0; i--) {
-
-            for (var b = 0; b < array[i].length; b++) {
-                int additive = isBigger(array, i + 1, b);
-                array[i][b] = array[i][b] + additive;
+    
+    // Small for left
+    int calculatorSmallLeft(int[][] pyramid) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(pyramid.length);
+        for(int i =0; i<pyramid.length; i++) {
+            sums.add(new ArrayList());
+            int b=i;
+            while (b>=0) {
+                sums.get(i).add(pyramid[((pyramid.length-1)-(b))][((pyramid[i].length-1 ) -b)]);
+   
+                b -=1;
+                
             }
         }
-        return array[0][0];
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
 
-    }
-
-    int calculatorSmall(int[][] array) {
-
-        for (var i = (array.length - 2); i >= 0; i--) {
-
-            for (var b = 0; b < array[i].length; b++) {
-                int additive = isSmaller(array, i + 1, b);
-                array[i][b] = array[i][b] + additive;
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isSmaller(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
             }
         }
-        return array[0][0];
+        
+        return sums.get(0).get(0);
     }
+    
+    int calculatorBigLeft(int[][] pyramid) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(pyramid.length);
+        for(int i =0; i<pyramid.length; i++) {
+            sums.add(new ArrayList());
+            int b=i;
+            while (b>=0) {
+                sums.get(i).add(pyramid[((pyramid.length-1)-(b))][((pyramid[i].length-1 ) -b)]);
+   
+                b -=1;
+                
+            }
+        }
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
+
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isBigger(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
+            }
+        }
+        
+        return sums.get(0).get(0);
+
+    }
+    
+    
+    // Small for right
+    int calculatorSmallRight(int[][] pyramid) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(pyramid.length);
+        for(int i =0; i<pyramid.length; i++) {
+            sums.add(new ArrayList());
+            int b=i;
+            while (b>=0) {
+                sums.get(i).add(pyramid[(pyramid.length-1)-b][(pyramid.length-1)- (pyramid[i].length-1)]);
+                
+                b -=1;
+                
+            }
+            
+        }
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
+
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isSmaller(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
+            }
+        }
+        
+        return sums.get(0).get(0);
+    }
+    
+    int calculatorBigRight(int[][] pyramid) {
+        ArrayList <ArrayList<Integer>> sums = new ArrayList<>(pyramid.length);
+        for(int i =0; i<pyramid.length; i++) {
+            sums.add(new ArrayList());
+            int b=i;
+            while (b>=0) {
+                sums.get(i).add(pyramid[(pyramid.length-1)-b][(pyramid.length-1)- (pyramid[i].length-1)]);
+                
+                b -=1;
+                
+            }
+            
+        }
+        
+        for (var i = (sums.size() - 2); i >= 0; i--) {
+
+            for (var b = 0; b < ( sums.get(i).size() ); b++) {
+                int additive = isBigger(sums.get(i+1), b);
+                sums.get(i).set(b,(sums.get(i).get(b) + additive) );
+            }
+        }
+        
+        return sums.get(0).get(0);
+    }
+    
 
 }
--- Dynamic/MainClass.java
+++ Dynamic/MainClass.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.ads7.Dynamic;
 
-/**
- *
- * @author START
- */
+
+
 public class MainClass {
     
     
@@ -18,24 +12,10 @@ public class MainClass {
             {3,8},
             {8,1,0},
             {2, 7, 4, 4},
-            {4, 5, 2, 6 ,5},    
+            {4, 5, 2, 6 ,5},
+            {5,10,20,8,3,0}
         };
-        
-        int[][] pyramidS = {
-            {7},
-            {3,8},
-            {8,1,0},
-            {2, 7, 4, 4},
-            {4, 5, 2, 6 ,5},    
-        };
-        int[][] pyramidB = {
-            {7},
-            {3,8},
-            {8,1,0},
-            {2, 7, 4, 4},
-            {4, 5, 2, 6 ,5},    
-        };
-        
+
         System.out.println("Первинний масив: ");
         for (var i = 0; i <pyramid.length; i++) {
             for (var b = 0; b < pyramid[i].length; b++) {
@@ -45,27 +25,23 @@ public class MainClass {
         }
     
         DataProcessor dp = new DataProcessor();
-        System.out.print("Найменша сума чисел на маршруті: ");
-        System.out.println(dp.calculatorSmall(pyramidS));
-        System.out.println("Найменші суми у всіх вершинах: ");
-        for (var i = 0; i <pyramidS.length; i++) {
-            for (var b = 0; b < pyramidS[i].length; b++) {
-                System.out.print( pyramidS[i][b] + " ");
-            }
-            System.out.println(" ");
-        }
+        System.out.print("Найменша сума чисел на маршруті 7: ");
+        System.out.println(dp.calculatorSmallMid(pyramid));
+
+        System.out.print("Найбільша сума чисел на маршруті 7: ");
+        System.out.println(dp.calculatorBigMid(pyramid)); 
         
+        System.out.print("Найменша сума чисел на маршруті 4: ");
+        System.out.println(dp.calculatorSmallLeft(pyramid));
+
+        System.out.print("Найбільша сума чисел на маршруті 4: ");
+        System.out.println(dp.calculatorBigLeft(pyramid)); 
         
-        System.out.print("Найбільша сума чисел на маршруті: ");
-        System.out.println(dp.calculatorBig(pyramidB));
-        System.out.println("Найбільші суми у всіх вершинах: ");
-        for (var i = 0; i <pyramidB.length; i++) {
-            for (var b = 0; b < pyramidB[i].length; b++) {
-                System.out.print( pyramidB[i][b] + " ");
-            }
-            System.out.println(" ");
-        }
-        
+        System.out.print("Найменша сума чисел на маршруті 5: ");
+        System.out.println(dp.calculatorSmallRight(pyramid));
+
+        System.out.print("Найбільша сума чисел на маршруті 5: ");
+        System.out.println(dp.calculatorBigRight(pyramid)); 
         
     }
     
